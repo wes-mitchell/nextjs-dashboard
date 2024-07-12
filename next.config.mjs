c3fa2449fbe6af43d2401f3682c81d2c2c0a5a36
Wes Mitchell <wmdrums@gmail.com>
2024-07-12 08:51:35 -0500

    link element and config update

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
-
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        appDir: true,
+    },
+    reactStrictMode: true,
+};
 
 export default nextConfig;
