23dba23013dc942bba08b25f024a93ef3f2534a8
Wes Mitchell <wmdrums@gmail.com>
2024-07-12 11:45:53 -0500

    client and server side component hooks

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -12,16 +12,7 @@ import { LatestInvoiceResponse } from '@/types/responseTypes';
 
 export async function fetchRevenue() {
   try {
-    // Artificially delay a response for demo purposes.
-    // Don't do this in production :)
-
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
-
     const data = await sql<Revenue>`SELECT * FROM revenue`;
-
-    // console.log('Data fetch completed after 3 seconds.');
-
     return data.rows;
   } catch (error) {
     console.error('Database Error:', error);
